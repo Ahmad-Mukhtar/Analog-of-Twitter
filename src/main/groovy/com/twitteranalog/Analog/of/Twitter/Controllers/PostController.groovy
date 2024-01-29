6687b696fb546bee3e793b02f4f89fb3e6ff8452
Usama Zaheer <usma.zaheer@cpqfinland.onmicrosoft.com>
2024-01-29 19:30:24 +0500

    Implemented Post Controller

--- src/main/groovy/com/twitteranalog/Analog/of/Twitter/Controllers/PostController.groovy
+++ src/main/groovy/com/twitteranalog/Analog/of/Twitter/Controllers/PostController.groovy
@@ -6,6 +6,9 @@ import com.twitteranalog.Analog.of.Twitter.Services.PostService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.PatchMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -24,5 +27,26 @@ class PostController {
         Post createdPost = postService.createPost(post)
         return ResponseEntity.status(HttpStatus.CREATED).body(createdPost);
     }
+
+    @DeleteMapping("/deletePost/{postId}")
+    ResponseEntity<?> deleteUser(@PathVariable(name = "postId") String postId) {
+       if( postService.deletePost(postId)==null)
+       {
+           return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Post Id not found: " + postId);
+       }
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+    @PatchMapping("/updatePost/{postId}")
+    ResponseEntity<?> updatePost(@PathVariable(name = "postId") String postId, @RequestBody Post post) {
+        Post updatedPost = postService.updatePost(postId,post)
+        if (updatedPost == null)
+        {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Post not found with ID: " + post.getId());
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(updatedPost);
+    }
+
+
 
 }
